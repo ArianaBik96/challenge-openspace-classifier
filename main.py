c116607b63f0cf808287781be9ab10f6ba1370b6
Ariana Bik <arianabik1996@hotmail.com>
2024-02-09 15:35:54 +0100

    - changed variable

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ list_colleagues = load_file(filename) # Load the list of colleagues from the fil
 random.shuffle(list_colleagues) # Shuffle the names in the list randomly
 
 
-tables = [] # Initialize an empty list for tables
-openspace = Openspace(tables, 6) # Initialize Openspace object with 6 tables
+room = [] # Initialize an empty list for tables
+openspace = Openspace(room, 6) # Initialize Openspace object with 6 tables
 openspace.organize(list_colleagues) # Organize the colleagues into tables
 openspace.display() # Display the arrangement of colleagues in tables
 openspace.store("sorted_tables.txt", list_colleagues) # Store the arrangement of tables with occupants into a file
